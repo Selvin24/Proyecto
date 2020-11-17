041c6d343f4666468a3a954aebe0b98ab105e2ca
Selvin24 <selvinpineda_2000@hotmail.com>
2020-11-17 13:03:19 -0600

    Vueles disponibles
    El filtron aun no se a realizado

---
+++ js/categoriaApp.js
@@ -0,0 +1,89 @@
+app.directive("categoriaApp", function(){
+    return { 
+        restrict: 'E', 
+        scope: { 
+          
+        }, 
+        templateUrl: 'js/temple/categoria.html',
+        link: function(scope, element, attrs) {       
+      
+      scope.vuelos=[
+        {
+          destino:'New York',
+          costo:'12,381',
+        },
+        {
+          destino:'Moscow',
+          costo:'47,186',
+        },
+        {
+          destino:'Berlin',
+          costo:'33,293',
+        },
+        {
+          destino:'Miami',
+          costo:'15,254',
+        },
+        {
+          destino:'Los Angeles',
+          costo:'12,819',
+        },
+        {
+          destino:'London',
+          costo:'15,926',
+        },
+        {
+          destino:'Paris',
+          costo:'14,268',
+        },
+        {
+          destino:'Orlando',
+          costo:'14,774',
+        },
+        {
+          destino:'Cancun',
+          costo:'19,141',
+        },
+        {
+          destino:'Madrid',
+          costo:'31,742',
+        },
+        {
+          destino:'Medellín',
+          costo:'15,574',
+        },
+        {
+          destino:'Houston',
+          costo:'13,907',
+        },
+        {
+          destino:'Marruecos',
+          costo:'77,284',
+        },
+        {
+          destino:'Barcelona',
+          costo:'30,176',
+        },        
+        {
+          destino:'Taipei',
+          costo:'42,840',
+        },
+        {
+          destino:'Dubai',
+          costo:'43,751',
+        },
+        {
+          destino:'Seattle',
+          costo:'20,710',
+        },
+        {
+          destino:'Munich',
+          costo:'32,323',
+        },
+      ],       
+      scope.categorias = function(index) { 
+        scope.buttonText = scope.articles[index].nombre;
+      }
+    }
+      };
+})
--- js/controller/info.js
+++ js/controller/info.js
@@ -1,9 +1,3 @@
-// app.controller('info', function ($scope, $http,$routeParams) {
-//   $http.get('./js/services/info.json').then(function (response) {
-//        $scope.puentes = response.data.puentes;
-//   });
-// });
-
 app.controller('info', ['$scope', 'services', '$routeParams', function($scope, services, $routeParams) {
   services.success(function(data) {
     $scope.puentes = data[$routeParams.id];
