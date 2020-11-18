209e0fc1d18745691f00cedf96bb984b665f9344
Selvin24 <selvinpineda_2000@hotmail.com>
2020-11-18 13:54:17 -0600

    Diseño
    Diseño y funcion de los filtros y categoria

--- js/controller/bridges.js
+++ js/controller/bridges.js
@@ -1,5 +1,9 @@
 app.controller('bridges', function ($scope, $http) {
      $http.get('./js/services/info.json').then(function (response) {
           $scope.myData = response.data;
+          
+          $scope.categoryFun = function(index) {
+               $scope.category = $scope.myData[index].categoria;     
+           }
      });
 });
