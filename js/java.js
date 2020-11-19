dcfc56009d0a4fdb8b05cfdd511bb5970f1e0302
Selvin24 <selvinpineda_2000@hotmail.com>
2020-11-19 13:15:03 -0600

    Comentario
    Eliminacion de codigo fail y comentario del codigo

--- js/java.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){ 
-	$('.solo-numero').numeric();
-   });
--- js/services/services.js
+++ js/services/services.js
@@ -1,21 +1,4 @@
 app.factory('services', function ($http) {
-    // return {
-    //     obtenerpuentes: function () {
-    //         var oPromise = $http.get('./js/services/info.json').then(function (response) {
-    //             return response.data;
-    //         });
-    //         return oPromise;
-    //     }
-    // };
-
-    // return $http.get('./js/services/info.json');
-    // var obj = {content:null};
-    // $http.get('./js/services/info.json').success(function(data) {
-    //     obj.content = data;
-    // });    
-
-    // return obj;    
-
     return $http.get('./js/services/info.json')
     .success(function(data) {
       return data;
